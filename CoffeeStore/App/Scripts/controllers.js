6af931d32aabd87f211012a459c9cea6dfd19ee8
Zhao Liu <liuzhao070@gmail.com>
2016-03-23 18:17:03 -0400

    Add the Add folder to make it as a client side application

---
+++ CoffeeStore/App/Scripts/controllers.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+angular.module('coffeeStoreApp')
+    .controller('IndexController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
+        $scope.message = "hello";
+        menuFactory.getProducts().then(function (data) {
+            $scope.products = data;
+        });
+
+    }])
+
+
+;
---
+++ CoffeeStore/App/app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('coffeeStoreApp', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $stateProvider
+
+    //route for the home page
+    .state('app', {
+        url: '/',
+        views: {
+            'header': {
+                templateUrl: 'Views/header.html',
+            },
+            'content': {
+                templateUrl: 'Views/home.html',
+                controller: 'IndexController'
+            },
+            'footer': {
+                templateUrl: 'Views/footer.html'
+            }
+        }
+    })
+
+});
