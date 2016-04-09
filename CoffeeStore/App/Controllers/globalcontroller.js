1610439abad56b095e7dd943d962d65595288a00
Zhao Liu <liuzhao070@gmail.com>
2016-04-09 18:28:03 -0400

    Implemnt the login part in front end, to login and store the token in front end.

--- CoffeeStore/App/Controllers/globalcontroller.js
+++ CoffeeStore/App/Controllers/globalcontroller.js
@@ -1,4 +1,5 @@
-﻿app.controller('GlobalController', function ($scope, $uibModal, $log) {
+﻿app.controller('GlobalController', ['$scope', 'authService', '$uibModal', '$log', '$state', function ($scope, authService, $uibModal, $log, $state) {
+    $scope.UserInfo = undefined;
     $scope.open = function () {
 
         var modalInstance = $uibModal.open({
@@ -7,28 +8,52 @@
             controller: 'ModalInstanceCtrl'
         });
 
-        modalInstance.result.then(function () {
-
+        modalInstance.result.then(function (result) {
+            $scope.UserInfo = result;
+            $state.go('app');
         }, function () {
             $log.info('Modal dismissed at: ' + new Date());
         });
     };
 
-});
+
+}]);
 
 // Please note that $uibModalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
 
-app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
+app.controller('ModalInstanceCtrl', ['$scope', 'authService', '$uibModalInstance', '$state', function ($scope, authService, $uibModalInstance, $state) {
+
+    $scope.user = {
+        "Username": "",
+        "Password": "",
+        "Ip": "232131",
+        "UserAgent": "dsd",
+        "Label": 23312
+    };
+
+    $scope.result = {
+        "Username": "",
+        "Token": "",
+    }
 
     $scope.ok = function () {
-        $uibModalInstance.close($scope.selected.item);
+        authService.login($scope.user).then(
+            function (data) {
+                $scope.result.Username = $scope.user.Username;
+                $scope.result.Token = data;
+            },
+            function (data) {
+
+            }
+        );
+        $uibModalInstance.close($scope.result);
     };
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
-});
+}]);
 
 app.controller('DropdownCtrl', function ($scope) {
 
---
+++ CoffeeStore/App/Services/AuthService.js
@@ -0,0 +1,58 @@
+﻿'use strict';
+
+angular.module('coffeeStoreApp')
+            .service('authService', ['$http', '$q', function ($http, $q) {
+                var Local_Token_Key = 'mykey';
+                var isAuthenticated = false;
+                var username = "";
+                var authToken;
+
+                function loadUserCredentials() {
+                    var token = window.localStorage.getItem(Local_Token_Key);
+                    if (token) {
+                        useCredentials(token);
+                    }
+                }
+
+                function storeUserCredentials(token) {
+                    window.localStorage.setItem(Local_Token_Key, token);
+                }
+
+                function useCredentials(token) {
+                    isAuthenticated = false;
+                    authToken = token;
+                }
+
+                function destoryUserCredentials() {
+                    authToken = undefined;
+                    isAuthenticated = false;
+
+                    window.localStorage.removeItem(Local_Token_Key);
+                }
+
+                var register = function (user) {
+
+                }
+
+                var login = function (user) {
+                    var deferred = $q.defer();
+                    $http.post('../api/login', user).success(function (data) {
+                        deferred.resolve(data);
+                    });
+                    return deferred.promise;
+                }
+
+                var logout = function () {
+                    destoryUserCredentials();
+                }
+
+                loadUserCredentials();
+
+                return {
+                    login: login,
+                    register: register,
+                    logout: logout,
+                    isAuthenticated: function () { return isAuthenticated; },
+                };
+
+            }]);
