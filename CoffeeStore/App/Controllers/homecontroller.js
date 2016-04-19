54d31ac16c7b9b228e121f7be31a9eb3ac43d468
Zhao Liu <liuzhao070@gmail.com>
2016-04-19 18:53:18 -0400

    Add a carousel in home view page

--- CoffeeStore/App/Controllers/homecontroller.js
+++ CoffeeStore/App/Controllers/homecontroller.js
@@ -2,12 +2,24 @@
 
 angular.module('coffeeStoreApp')
     .controller('IndexController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
-        $scope.message = "Hello Baodan!!!";
         menuFactory.getProducts().then(function (data) {
             $scope.products = data;
         });
 
-    }])
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        
+        $scope.addSlide = function () {
+            var newWidth = 1200 +slides.length + 1;
+            slides.push({
+                image: '//lorempixel.com/' + newWidth + '/400/food',
+                text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
+                  ['Pic', 'Pic', 'Pic', 'Pic'][slides.length % 4]
+            });
+        };
+        for (var i = 0; i < 4; i++) {
+            $scope.addSlide();
+        }
 
-
-;
+    }]);
