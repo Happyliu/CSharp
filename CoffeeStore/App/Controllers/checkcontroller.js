ef68254e58d05877520f009ed34189ca22fab807
Zhao Liu <liuzhao070@gmail.com>
2016-05-26 23:06:34 -0400

    Add Checkout html and js file

--- CoffeeStore/App/Controllers/checkcontroller.js
+++ CoffeeStore/App/Controllers/checkcontroller.js
@@ -2,5 +2,18 @@
 
 angular.module('coffeeStoreApp')
     .controller('CheckController', ['$scope', function ($scope) {
-
+        $scope.state = "FL";
+        $scope.country = "US";
+        $scope.states = ["FL", "CA", "TX", "NM", "AZ"];
+        $scope.countries = ["US", "UK", "China", "Canda", "India"];
+        $scope.checkout = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            addressline1: "",
+            addressline2: ""
+        };
+        $scope.checkout = function () {
+            alert("hello");
+        }
     }]);
